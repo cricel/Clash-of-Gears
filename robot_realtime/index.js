071219fd0b90fb923014a1f9f8fa59a07d262a5a
kPatch <irvsteve@gmail.com>
2020-10-22 02:55:25 -0400

    Add template functions

--- robot_realtime/index.js
+++ robot_realtime/index.js
@@ -6,6 +6,7 @@ firebase.initializeApp(config);
 // firebase.analytics();
 
 var isMovement = false;
+var movementVal = 0;
 var isMotor1 = false;
 var isMotor2 = false;
 var isMotor3 = false;
@@ -32,8 +33,10 @@ firebase.database().ref('player1/motor1').on('value', function(snapshot) {
 
     if(snap == "OPEN") {
         isMotor1 = true;
+        moveMotor1(isMotor1);
     } else {
         isMotor1 = false;
+        moveMotor1(isMotor1);
         console.log("MOTOR1: " + snap);
     }    
 });
@@ -43,8 +46,10 @@ firebase.database().ref('player1/motor2').on('value', function(snapshot) {
 
     if(snap == "OPEN") {
         isMotor2 = true;
+        moveMotor2(isMotor2);
     } else {
         isMotor2 = false;
+        moveMotor2(isMotor2);
         console.log("MOTOR2: " + snap);
     } 
 });
@@ -54,25 +59,49 @@ firebase.database().ref('player1/motor3').on('value', function(snapshot) {
 
     if(snap == "OPEN") {
         isMotor3 = true;
+        moveMotor3(isMotor3);
     } else {
         isMotor3 = false;
+        moveMotor3(isMotor3);
         console.log("MOTOR3: " + snap);
     } 
 });
 
-const Gpio = require('pigpio').Gpio;
-const motor = new Gpio(10, {mode: Gpio.OUTPUT});
- 
-let pulseWidth = 1000;
-let increment = 100;
- 
+
+// This is while look, it will run forever
 setInterval(() => {
-  motor.servoWrite(pulseWidth);
+    if(isMovement) {
+        // Move both motors
+    }
+}, 10);
+
+
+function moveMotor1(isOpen) {
+
+}
+
+function moveMotor2(isOpen) {
+
+}
+
+function moveMotor3(isOpen) {
+
+}
+
+
+// const Gpio = require('pigpio').Gpio;
+// const motor = new Gpio(10, {mode: Gpio.OUTPUT});
  
-  pulseWidth += increment;
-  if (pulseWidth >= 2000) {
-    increment = -100;
-  } else if (pulseWidth <= 1000) {
-    increment = 100;
-  }
-}, 1000);
+// let pulseWidth = 1000;
+// let increment = 100;
+ 
+// setInterval(() => {
+//   motor.servoWrite(pulseWidth);
+ 
+//   pulseWidth += increment;
+//   if (pulseWidth >= 2000) {
+//     increment = -100;
+//   } else if (pulseWidth <= 1000) {
+//     increment = 100;
+//   }
+// }, 1000);
