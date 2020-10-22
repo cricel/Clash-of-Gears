4c9efc00d59a0d8ae0f682d176d19fd2335fe06d
cricel <cricel.design@gmail.com>
2020-10-22 03:19:42 -0400

    robot firebase control

--- robot_realtime/index.js
+++ robot_realtime/index.js
@@ -11,6 +11,31 @@ var isMotor1 = false;
 var isMotor2 = false;
 var isMotor3 = false;
 
+
+//-------------
+const Gpio = require('pigpio').Gpio;
+const readline = require('readline');
+const L298N = require('./node_modules/pigpio-l298n/l298n.js');
+
+/////////     servo motor      ////////////
+const left_motor = new Gpio(16, {mode: Gpio.OUTPUT});
+const right_motor = new Gpio(20, {mode: Gpio.OUTPUT});
+const bottom_motor = new Gpio(21, {mode: Gpio.OUTPUT});
+
+// true for close, false for open
+var left_motor_motion = true; 
+var right_motor_motion = true;
+var bottom_motor_motion = true;
+/////////     ^^^^^^^^^^^^      ////////////
+
+/////////     wheel motor      ////////////
+let l298n = new L298N(25,23,24,22,17,27);
+l298n.setSpeed(l298n.NO1,60);
+l298n.setSpeed(l298n.NO2,60);
+/////////     ^^^^^^^^^^^^      ////////////
+//--------------------------
+
+
 // firebase.database().ref('player1/movement').once('value').then( function(snapshot) {
 //     var snap = JSON.stringify(snapshot.val())
 //     console.log("SNAP: " + snap);
@@ -22,7 +47,7 @@ firebase.database().ref('player1/movement').on('value', function(snapshot) {
     movementVal = snap;
     console.log("SNAP: " + snap);
 
-    if(snap = "STOP") {
+    if(snap == "STOP") {
         isMovement = false;
     } else {
         isMovement = true;
@@ -72,37 +97,38 @@ firebase.database().ref('player1/motor3').on('value', function(snapshot) {
 // This is while look, it will run forever
 setInterval(() => {
     if(isMovement) {
-        // Move both motors
+        if (movementVal === 'W') {
+            l298n.forward(l298n.NO1);
+            l298n.forward(l298n.NO2);
+        } else if (movementVal === 'A') {
+            l298n.backward(l298n.NO1);
+            l298n.forward(l298n.NO2);
+        } else if (movementVal === 'D') {
+            l298n.forward(l298n.NO1);
+            l298n.backward(l298n.NO2);
+        } else if (movementVal === 'S') {
+            l298n.backward(l298n.NO1)
+            l298n.backward(l298n.NO2)
+        }
+    }
+    else{
+        l298n.stop(l298n.NO1);
+        l298n.stop(l298n.NO2);
     }
 }, 10);
 
 
 function moveMotor1(isOpen) {
-
+    if (isOpen){left_motor.servoWrite(1000);}
+    else{left_motor.servoWrite(1800);}
 }
 
 function moveMotor2(isOpen) {
-
+    // if (isOpen){right_motor.servoWrite(1000);}
+    // else{right_motor.servoWrite(500);}
 }
 
 function moveMotor3(isOpen) {
-
+    if (isOpen){bottom_motor.servoWrite(1000);}
+    else{bottom_motor.servoWrite(500);}
 }
-
-
-// const Gpio = require('pigpio').Gpio;
-// const motor = new Gpio(10, {mode: Gpio.OUTPUT});
- 
-// let pulseWidth = 1000;
-// let increment = 100;
- 
-// setInterval(() => {
-//   motor.servoWrite(pulseWidth);
- 
-//   pulseWidth += increment;
-//   if (pulseWidth >= 2000) {
-//     increment = -100;
-//   } else if (pulseWidth <= 1000) {
-//     increment = 100;
-//   }
-// }, 1000);
